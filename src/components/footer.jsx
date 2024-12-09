82916ef912011fd638131faf57391845cf2c604c
Airam Santos diaz <airam@MacBook-Pro-de-Airam.local>
2024-12-09 20:30:05 +0100

    footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import "./footer.css"
+function Footer(){
+    return(
+
+
+  <footer className="footer">
+<a className="=link" href="https://github.com/martxgomez/travels-app.git">coperate with us</a>
+
+  </footer>
+    )
+}
+
+export default Footer;
