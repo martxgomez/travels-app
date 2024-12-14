e0c87c71504bb2821ce42b360f1d477039497483
Airam Santos diaz <airam@MacBook-Pro-de-Airam.local>
2024-12-14 10:58:21 +0100

    css community

--- src/pages/TravelsCommunity.jsx
+++ src/pages/TravelsCommunity.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import supabase from "../supabase/config";
-
+import "./TravelsCommunity.css"
 
 
 const TravelsCommunity = () => {
