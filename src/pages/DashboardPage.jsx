a1b37c230db9c646467f3326f5b5975832b5455a
Andrea Bronzoni <bronzoni.andrea@libero.it>
2024-12-16 21:23:31 +0100

    filter by city on DashboardPage implemented and working

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import "./DashboardPage.css"
 import TravelList from "../components/travels/TravelList"
 
 function Dashboardpage ({travels}) {
 
+    const [search, setSearch] = useState("");
 
+        const handleSearch = (event) => {
+            setSearch(event.target.value);
+        };
+        const filteredTravels = travels.filter((travel)=> travel.destination.toLowerCase().includes(search.toLowerCase())
+);
         return (
            
             <div>
                 <h2>Travels List</h2>
-                <TravelList travels={travels} />
+                <input
+                    type="text"
+                    placeholder="Search by destination..."
+                    value={search}
+                    onChange={handleSearch}
+                    />
+                <TravelList travels={filteredTravels} />
             </div>
         );
      }
