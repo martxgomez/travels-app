bb78db2d45c83b7c87252aa7d925fcafac0ee312
Marta Gómez <martagomezmartinez95@gmail.com>
2024-12-18 21:25:30 +0100

    my travels list in progress

--- src/components/travels/MyTravelsList.jsx
+++ src/components/travels/MyTravelsList.jsx
@@ -1,20 +1,61 @@
 import TravelCard from "./TravelCard";
-function TravelList({ travels, setTravels, condition }) {
-    
-  const filteredTravels = travels.filter((travel) => travel[condition]);
-console.log(filteredTravels);
+import { useEffect } from "react";
+import { useState } from "react";
+import supabase from "../../supabase/config";
 
-  return (
-    <>
-      {filteredTravels.lenght>0 ? (
-        filteredTravels.map((travel) => {
-          return <TravelCard key={travel.id} setTravels={setTravels} />;
-        })
-      ) : (
+function TravelList() {
+  const [favs, setFavs] = useState([]);
+  const [myTrips, setMyTrips] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchAllTrips = async () => {
+      try {
+        //get from favs and my trips
+        const { data: favsData, error: favsError } = await supabase
+          .from("favs")
+          .select("*");
+        const { data: myTripsData, error: myTripsError } = await supabase
+          .from("mytrips")
+          .select("*");
+
+        if (favsError || myTripsError) throw favsError || myTripsError;
+
+        setFavs(favsData);
+        setMyTrips(myTripsData);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchAllTrips();
+  }, []);
+
+  if (loading) return <p className="my-trips-loading">Cargando...</p>;
+  if (error) return <p className="my-trips-error">Error: {error}</p>;
+  if (favs.length===0 && myTrips.length===0) {
+    return (
+      <>
         <p>No favorite trips found 🧳❤️</p>
-      )}
-    </>
-  );
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>
+          {favs.map((fav) => {
+            return <TravelCard key={fav.id} {...fav} />;
+          })}
+          {myTrips.map((myTrip) => {
+            return <TravelCard key={myTrip.id} {...myTrip} />;
+          })}
+        </div>
+      </>
+    );
+  }
 }
 
 export default TravelList;
