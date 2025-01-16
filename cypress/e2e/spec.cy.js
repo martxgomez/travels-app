11872a2c928f27061bea9c30aef8248f56f2da07
martxgomez <martagomezmartinez95@gmail.com>
2025-01-16 21:11:00 +0100

    testing failed 2.0

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -14,22 +14,22 @@ describe("view a travel", () => {
   });
 });
 
-// describe('create a travel', () => {
-//   it('passes', () => {
-//     cy.visit('https://travelers-app.netlify.app')
-//     cy.get('.navbar').contains("My trips").click()
-//     cy.get('.my-trips-container').contains("+").click()
-//     cy.get(".form").should("be.visible");
-//     cy.get('input').eq(0).type("Valencia")
-//     cy.get('input').eq(1).type("400")
-//     cy.get('input').eq(2).type("https://www.spain.info/export/sites/segtur/.content/imagenes/cabeceras-grandes/valencia/ciudad-artes-ciencias-valencia-c-luca-bravo-u-UyUjtbu5vj4.jpg")
-//     cy.get('input').eq(3).type("4")
-//     cy.get('input').eq(4).type("4.2")
-//     cy.get('input').eq(5).type("going to Malvarrosa{enter}")
-//     cy.get('input').eq(6).type("La Ciudad de las Artes y las Ciencias{enter}")
-//     cy.get('button').click()
-//   })
-// })
+describe('create a travel', () => {
+  it('passes', () => {
+    cy.visit('https://travelers-app.netlify.app')
+    cy.get('.navbar').contains("My trips").click()
+    cy.get('.my-trips-container').contains("+").click()
+    cy.get(".form").should("be.visible");
+    cy.get('input').eq(0).type("Valencia")
+    cy.get('input').eq(1).type("400")
+    cy.get('input').eq(2).type("https://www.spain.info/export/sites/segtur/.content/imagenes/cabeceras-grandes/valencia/ciudad-artes-ciencias-valencia-c-luca-bravo-u-UyUjtbu5vj4.jpg")
+    cy.get('input').eq(3).type("4")
+    cy.get('input').eq(4).type("4.2")
+    cy.get('input').eq(5).type("going to Malvarrosa{enter}")
+    cy.get('input').eq(6).type("La Ciudad de las Artes y las Ciencias{enter}")
+    cy.get('button').click()
+  })
+})
 describe("edit a travel", () => {
   it("passes", () => {
     cy.visit("https://travelers-app.netlify.app");
